c8e14075d5412e6cc9cff14ff3be7d32de7445f7
AckeardOct <AckeardOct@yandex.ru>
2019-11-08 17:32:25 +0000

    add auto building lua deps

--- install-deps.sh
+++ install-deps.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-dir="./build/conan"
+root=$(pwd)
 
+echo "-----CONAN DEPS-----"
+dir="$root/build/conan"
 rm -rf $dir
 mkdir -p $dir
 cd $dir
 
 conan remote add bincrafters "https://api.bintray.com/conan/bincrafters/public-conan"
 conan install ../.. --build missing
+
+echo "-----LUA-----"
+dir="$root/build/lua"
+rm -rf $dir
+mkdir -p $dir
+cd $dir
+
+wget "https://www.lua.org/ftp/lua-5.3.5.tar.gz"
+tar xzf lua*.tar.gz
+rm -rf lua*.tar.gz
+mkdir bin
+cd lua*
+make linux local
+cp -r "./install/bin" "../bin"
+cp -r "./install/include/" "../bin/"
