965e8903f05e162f4fc17d432af1ca54f662d422
AckeardOct <AckeardOct@yandex.ru>
2019-11-10 14:55:34 +0300

    simple entity

--- src/scenes/asteroids_scene.h
+++ src/scenes/asteroids_scene.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "i_scene.h"
+#include <entt/entity/registry.hpp>
+#include <vector>
+
+class IBasicSys;
+class IRenderSys;
 
 class AsteroidsScene : public IScene {
 public:
@@ -12,4 +17,14 @@ public:
     virtual void input(float dt, const SDL_Event& event) override;
     virtual void update(float dt) override;
     virtual void render(float dt) override;
+
+private:
+    void initBasicSystems();
+    void initRenderSystems();
+    void initEntities();
+
+private:
+    entt::registry reg;
+    std::vector<IBasicSys*> basicSystems;
+    std::vector<IRenderSys*> renderSystems;
 };
